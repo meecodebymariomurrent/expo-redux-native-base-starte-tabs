314b42fe77b9de312c07901abe5f5036b8a090f4
Mario Murrent <mario@murrent.at>
2022-06-30 16:02:23 +0200

    Switched to reduxjs/toolkit reducer setup

--- src/core/redux/store.ts
+++ src/core/redux/store.ts
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 
-import { counterReducer } from './reducers/counterReducer'
+import counterReducer from './reducers/counterReducer';
 
 const reducer = {
     counter: counterReducer
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,7 +1,6 @@
 import { connect, ConnectedProps } from 'react-redux';
 import { Component, ReactNode } from 'react';
 import { Container } from 'native-base';
-import { countUp, resetCounter } from '../core/redux/actions/counterAction';
 import { RootState } from '../core/redux/store';
 
 class Profile extends Component<CustomProps, State> {
@@ -14,12 +13,9 @@ class Profile extends Component<CustomProps, State> {
 }
 
 const mapStateToProps = (state: RootState, ownProps: OwnProps) => ({
-    counter: state.counter
 });
 
 const mapDispatchToProps = {
-    dispatchCountUp: countUp,
-    dispatchResetCounter: resetCounter
 }
 
 const connector = connect<CustomProps>(mapStateToProps, mapDispatchToProps)(Profile);
